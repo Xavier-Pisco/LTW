d484776829ada95a65dc2df64370ccf9cd386325
Xavier-Pisco <x.pisco@gmail.com>
2020-11-20 11:38:01 +0000

    Finished second 5

--- Praticas/JS/news/script.js
+++ Praticas/JS/news/script.js
@@ -1,10 +1,18 @@
 let commentForm = document.querySelector('form');
 
-function submitComment(event){
-	event.preventDefault(event);
-	console.log('Here');
-}
-
 commentForm.addEventListener('submit', function(event) {
 	submitComment(event);
 })
+
+function submitComment(event){
+	event.preventDefault(event);
+
+	let id = document.querySelector('form input[type=hidden]').value;
+	let comment_id = document.querySelector('.comment:last-of-type').attributes['data-id'].value;
+	let username = document.querySelector('form input[type=text]').value;
+	let comment = document.querySelector('form textarea').value;
+	console.log(id);
+	console.log(comment_id);
+	console.log(username);
+	console.log(comment);
+}
